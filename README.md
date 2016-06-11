fd8373fd2622016a2c515a5e556ffdc411c5f3a8
yreenchan <yreenchan@gmail.com>
2016-06-11 14:03:35 +0800

    fixed some bug.

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 ## About the easymath
 This plugin is the library of Math, that what you are always do in js.Now you can do some calculation with object of Math.
 
-## Why I wan't to do this?
-When I write css with postcss, I wan't postcss can do all the calculations in the css for me.Especially, I need a plugin can resolve the calculation which will return percentage value.
+## Why I want to do this?
+When I write css with postcss, I want postcss can do all the calculations in the css for me.Especially, I need a plugin can resolve the calculation which will return percentage value.
+
+## API
+Math.per accept a fixed value, that is equal with value.toFixed.
+
+## Usage
+```css
+h3 {
+	padding-top: Math.per(430/720, 3);
+	width: Math.round(100/50px);
+}
+```
--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 const postcss = require('postcss');
 
 function per(val) {
+	val = val.split(',');
+	return (resolve(val[0]) * 100).toFixed(val[1] || 5) + '%';
+}
+
+function resolve(val) {
 	try {
 		eval('var val = ' + val);
 	} catch(e) {
 		console.log('[Error] in Math.per ', e);
 	}
-	return val.toFixed(5) * 100 + '%';
+	return val;
 }
 
 
-var precision = postcss.plugin('postcss-precision', function() {
+module.exports = postcss.plugin('postcss-precision', function() {
 	var mathTest = /Math\.([a-z]+)\(([^\)]+)\)/;
+	var unitTest = /px|em|rem/;
 
 	return function(style) {
-		style.eachDecl(function(decl) {
+		style.walkDecls(function(decl) {
 			if (decl.value && mathTest.test(decl.value)) {
 				var rs = decl.value.match(mathTest);
 				if (rs) {
 					if (rs[1] === 'per' && rs[2]) {
 						decl.value = per(rs[2]);
 					} else {
-						decl.value = Math[rs[1]](rs[2])
+						var unit = rs[2].match(/px|em|rem/) || [''];
+						var val = rs[2].replace(unitTest, '');
+						decl.value = Math[rs[1]](resolve(val)) + unit;
 					}
 				}
 			}
-
 		});
 	};
 });
